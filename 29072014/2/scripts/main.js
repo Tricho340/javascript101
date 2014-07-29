2701d08461d798c3e3b833cf7d374ab1e524c3e8
Trifon Statkov <trifon@trimonstudio.com>
2014-07-29 21:04:34 +0300

    First draft of 2nd exercise from today

---
+++ 29072014/2/scripts/main.js
@@ -0,0 +1,9 @@
+require.config({
+  paths: {
+    "Q": "../bower_components/q/q",
+    "jquery": "../bower_components/jquery/dist/jquery.min"
+  }
+});
+require(["Q", "jquery"], function(Q, $) {
+
+});
